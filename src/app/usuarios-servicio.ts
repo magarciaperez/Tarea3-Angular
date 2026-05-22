eda3f5b7f5da94d51aa4b07bb52a2c6293556b9e
Miguel Angel Garcia <magarciaperez@gmail.com>
2026-05-22 13:18:12 +0200

    refactor(usuarios): limpiar comentarios y mover estilo de error a scss

--- src/app/usuarios-servicio.ts
+++ src/app/usuarios-servicio.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError, catchError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosServicio {
+
   private apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
   constructor(private http: HttpClient) { }
 
+  // Petición GET a la API. Si falla, se captura con catchError
   getUsuarios(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl)
-      .pipe(
-        catchError(this.handleError)
-      );
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      catchError(this.handleError)
+    );
   }
 
+  // Genera un Observable de error con un mensaje legible para el componente
   private handleError(error: any) {
     let mensaje = 'Error al obtener los usuarios';
-
     if (error.status === 0) {
       mensaje = 'No hay conexión con el servidor';
     }
-
     return throwError(() => new Error(mensaje));
   }
 }
